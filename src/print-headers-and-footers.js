142fd13d83fefe252bb8a02696f9219db636e138
Jordan Last <jordan.michael.last@gmail.com>
2015-12-22 22:03:21 -0700

    margins seem to be working

--- src/print-headers-and-footers.js
+++ src/print-headers-and-footers.js
@@ -3,21 +3,64 @@
 var PrintHAF = (function() {
 
 	var o = {};
-	
-	var headerTemplate = '';
-	var footerTemplate = '';
-	var regionHeight = 0;
-	var regionWidth = 0;
+
+	var width = 0;
+	var height = 0;
+	var marginTop = 0;
+	var marginBottom = 0;
 	var marginLeft = 0;
 	var marginRight = 0;
 	
-	o.init = function(options) {
+	var createHeaderTemplate = function() {};
+	var createFooterTemplate = function() {};
+	
+	o.init = function(options) {	
+		if (options.size) {
+			
+			if (options.size === 'letter') {
+				width = 8.5 * 96;
+				height = 11 * 96;
+			}
+			
+		}
+		else {
+			options.width && (width = options.width);
+			options.height && (height = options.height);
+		}
+		
+		options.marginTop && (marginTop = options.marginTop);
+		options.marginBottom && (marginTop = options.marginBottom);
+		options.marginLeft && (marginTop = options.marginLeft);
+		options.marginRight && (marginTop = options.marginRight);
+		
+		options.createHeaderTemplate && (createHeaderTemplate = options.createHeaderTemplate);
+		options.createFooterTemplate && (createFooterTemplate = options.createFooterTemplate);
+	};
+	
+	/*o.init = function(options) {
 		headerTemplate = options.headerTemplate();
 		footerTemplate = options.footerTemplate();
 		regionHeight = calculateRegionHeight(calculateRenderedHeight(headerTemplate), calculateRenderedHeight(footerTemplate), options.marginTop, options.marginBottom, options.size);
 		regionWidth = calculateRegionWidth(options.size);
 		marginLeft = options.marginLeft;
 		marginRight = options.marginRight;
+	};*/
+	
+	o.print = function() {
+		var headerTemplate = createHeaderTemplate();
+		var footerTemplate = createFooterTemplate();
+		var regionHeight = calculateRegionHeight(calculateRenderedHeight(headerTemplate), calculateRenderedHeight(footerTemplate), marginTop, marginBottom, height);
+		var regionWidth = calculateRegionWidth(marginLeft, marginRight, width);
+		
+		var regionContainer = document.createElement('div')
+		var mainContainer = document.querySelector('.haf-main-container');
+		var printContainer = document.querySelector('.haf-print-container');
+		
+		before(mainContainer, regionContainer, printContainer);
+		prepare(mainContainer, regionContainer, headerTemplate, footerTemplate, regionHeight, regionWidth, marginTop, marginBottom, marginLeft, marginRight).then(function() {
+			window.print();
+			after(mainContainer, regionContainer, printContainer);
+		});
 	};
 	
 	var calculateRenderedHeight = function(template) {
@@ -35,32 +78,12 @@ var PrintHAF = (function() {
 		return height;
 	};
 	
-	var calculateRegionHeight = function(headerHeight, footerHeight, marginTop, marginBottom, size) {
-		
-		if (size === 'letter') {
-			return 11 * 96 - (headerHeight + footerHeight + marginTop + marginBottom);
-		}		
-		
+	var calculateRegionHeight = function(headerHeight, footerHeight, marginTop, marginBottom, height) {
+		return height - (headerHeight + footerHeight + marginTop + marginBottom);	
 	};
 	
-	var calculateRegionWidth = function(size) {
-		
-		if (size === 'letter') {
-			return 8.5 * 96;
-		}
-		
-	};
-	
-	o.print = function() {
-		var regionContainer = document.createElement('div')
-		var mainContainer = document.querySelector('.haf-main-container');
-		var printContainer = document.querySelector('.haf-print-container');
-		
-		before(mainContainer, regionContainer, printContainer);
-		prepare(mainContainer, regionContainer, headerTemplate, footerTemplate, regionHeight, regionWidth, marginLeft, marginRight).then(function() {
-			window.print();
-			after(mainContainer, regionContainer, printContainer);
-		});
+	var calculateRegionWidth = function(marginLeft, marginRight, width) {
+		return width - (marginLeft + marginRight);
 	};
 	
 	var before = function(mainContainer, regionContainer, printContainer) {
@@ -69,9 +92,9 @@ var PrintHAF = (function() {
 		document.body.appendChild(regionContainer);
 	};
 	
-	var prepare = function(mainContainer, regionContainer, headerTemplate, footerTemplate, regionHeight, regionWidth, marginLeft, marginRight) {
+	var prepare = function(mainContainer, regionContainer, headerTemplate, footerTemplate, regionHeight, regionWidth, marginTop, marginBottom, marginLeft, marginRight) {
 		return new Promise(function(resolve, reject) {
-			var prepareForRendering = function(template, regionWidth, marginLeft, marginRight) {
+			var prepareForRendering = function(templateType, template, regionWidth, marginTop, marginBottom, marginLeft, marginRight) {
 				
 				var element = document.createElement('div');
 				element.innerHTML = template;
@@ -82,6 +105,14 @@ var PrintHAF = (function() {
 				element.style.height = Math.max(element.clientHeight, element.scrollHeight, element.offsetHeight); //TODO Without this line, why is the height of the template a decimal pixel value? Figure out how to fix that
 				element.parentNode.removeChild(element);
 				
+				if (templateType === 'header') {
+					element.style.paddingTop = marginTop + 'px';
+				}
+				
+				if (templateType === 'footer') {
+					element.style.paddingBottom = marginBottom + 'px';
+				}
+				
 				element.style.width = regionWidth + 'px';
 				element.style.paddingLeft = marginLeft + 'px';
 				element.style.paddingRight = marginRight + 'px';
@@ -89,7 +120,7 @@ var PrintHAF = (function() {
 				return element;
 			};
 			
-			prepareRegions(mainContainer, regionContainer, prepareForRendering(headerTemplate, regionWidth, marginLeft, marginRight), prepareForRendering(footerTemplate, regionWidth, marginLeft, marginRight), regionHeight, regionWidth).then(function() {
+			prepareRegions(mainContainer, regionContainer, prepareForRendering('header', headerTemplate, regionWidth, marginTop, marginBottom, marginLeft, marginRight), prepareForRendering('footer', footerTemplate, regionWidth, marginTop, marginBottom, marginLeft, marginRight), regionHeight, regionWidth).then(function() {
 				resolve();
 			});
 		});
